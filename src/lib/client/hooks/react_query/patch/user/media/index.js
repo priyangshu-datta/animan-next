b0a1a8f07d7f1866057fcd0ab7c5afb0ba22c757
Priyangshu Datta <84124979+priyangshu-datta@users.noreply.github.com>
2025-05-28 01:55:27 +0530

    removed redundant code

--- src/lib/client/hooks/react_query/patch/user/media/index.js
+++ src/lib/client/hooks/react_query/patch/user/media/index.js
@@ -2,28 +2,25 @@ import { rpcRequest } from '@/lib/client/api-clients/rpc-client';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export function useUpdateUserMedia({
-  mediaId,
-  mediaType,
   handleSuccess = () => {},
   handleError = () => {},
 } = {}) {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: ({ mediaId, ...patchData }) => {
-      return rpcRequest({
+    mutationFn: ({ mediaId, mediaType, ...patchData }) =>
+      rpcRequest({
         action: 'patch:user:media:{mediaId,patchData}',
-        context: { mediaId, ...patchData },
-      });
-    },
-    onSuccess: () => {
+        context: { mediaId, mediaType, ...patchData },
+      }),
+    onSuccess: (_, variables) => {
       queryClient.invalidateQueries({
         predicate: (query) =>
           ([
             'get:media:full-details:{mediaId,mediaType}',
             'get:user:media:{mediaId,mediaType}',
           ].includes(query.queryKey[0]) &&
-            query.queryKey[1] === mediaId &&
-            query.queryKey[2] === mediaType) ||
+            query.queryKey[1] === variables.mediaId &&
+            query.queryKey[2] === variables.mediaType) ||
           query.queryKey[0] ===
             'get:user:media:list:{mediaType,mediaListStatus}',
       });

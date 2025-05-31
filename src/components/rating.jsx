cff97b97529ca3d09437057b2805c73598ced4c8
Priyangshu Datta <84124979+priyangshu-datta@users.noreply.github.com>
2025-05-31 21:16:51 +0530

    custom rating component

---
+++ src/components/rating.jsx
@@ -0,0 +1,25 @@
+import AppStorage from '@/utils/local-storage'
+import { Rating as YamadaRating, Tooltip } from '@yamada-ui/react';
+import { useEffect } from 'react';
+
+export function Rating({ score, stars = 5, maxScore = 10, label = '' }) {
+  const normalizedScore = (stars * score) / maxScore;
+  let locale;
+  useEffect(() => {
+    locale = AppStorage.get('locale');
+  }, []);
+  return (
+    <Tooltip
+      label={`${label}${new Intl.NumberFormat(locale, {
+        style: 'percent',
+      }).format(normalizedScore / stars)}`}
+    >
+      <YamadaRating
+        readOnly
+        value={normalizedScore}
+        fractions={maxScore}
+        items={stars}
+      />
+    </Tooltip>
+  );
+}

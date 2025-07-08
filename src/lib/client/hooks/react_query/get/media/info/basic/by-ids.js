48baea5dd9aa5a9c1ecfb11c3a143c808e6aa0c7
Priyangshu Datta <84124979+priyangshu-datta@users.noreply.github.com>
2025-07-08 19:20:38 +0530

    fix: add optional chaining to mediaIds sorting

--- src/lib/client/hooks/react_query/get/media/info/basic/by-ids.js
+++ src/lib/client/hooks/react_query/get/media/info/basic/by-ids.js
@@ -2,7 +2,7 @@ import { rpcRequest } from '@/lib/client/api-clients/rpc-client';
 import { useQuery } from '@tanstack/react-query';
 
 export function useMediaBasicDetailsByIds({ mediaIds, mediaType }) {
-  mediaIds.sort();
+  mediaIds?.sort();
   return useQuery({
     queryKey: [
       'get:media:basic-details:{mediaIds,mediaType}',

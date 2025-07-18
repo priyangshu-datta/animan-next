7af53583501d2495b7646883562d99e25ba8aabf
Priyangshu Datta <84124979+priyangshu-datta@users.noreply.github.com>
2025-07-18 11:53:36 +0530

    added nuqs to better manage states in url

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,26 +1,28 @@
 'use client';
 
 import { makeQueryClient } from '@/lib/client/api-clients/query-client';
-import { AppProgressProvider as ProgressProvider } from '@bprogress/next'
+import { AppProgressProvider as ProgressProvider } from '@bprogress/next';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { useState } from 'react';
 
 const Providers = ({ children }) => {
   const [queryClient] = useState(() => makeQueryClient());
 
   return (
-    <ProgressProvider
-      height="4px"
-      options={{ showSpinner: false,  }}
-      shallowRouting
-      
-    >
-      <QueryClientProvider client={queryClient}>
-        {children}
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </ProgressProvider>
+    <NuqsAdapter>
+      <ProgressProvider
+        height="4px"
+        options={{ showSpinner: false }}
+        shallowRouting
+      >
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ProgressProvider>
+    </NuqsAdapter>
   );
 };
 
